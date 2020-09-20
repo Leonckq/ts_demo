5fdb5c481bf4a7bd0107b4ff06a7b5410fd9139d
leon <liyang@hosecloud.com>
2020-09-20 13:20:22 +0800

    feat: ts第一次提交

---
+++ build/webpack.config.js
@@ -0,0 +1,7 @@
+const { merge } = require('webpack-merge')
+const baseConfig = require('./webpack.base.config')
+const devConfig  = require('./webpack.dev.config')
+const proConfig = require('./webpack.pro.config')
+console.log(merge)
+const config = process.NODE_ENV === 'development' ? devConfig : proConfig
+module.exports = merge(baseConfig, config)
